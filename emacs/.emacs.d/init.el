fd7a946b9fb82b383ae78efca50daac07c246fdf
ntkomata <ntkomata@outlook.com>
2018-12-11 03:15:37 +0800

    sth

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -34,6 +34,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("f0dc4ddca147f3c7b1c7397141b888562a48d9888f1595d69572db73be99a024" "93a0885d5f46d2aeac12bf6be1754faa7d5e28b27926b8aa812840fe7d0b7983" "6d589ac0e52375d311afaa745205abb6ccb3b21f6ba037104d71111e7e76a3fc" "4697a2d4afca3f5ed4fdf5f715e36a6cac5c6154e105f3596b44a4874ae52c45" default)))
  '(org-default-notes-file (concat org-directory "/notes.org"))
  '(org-directory "~/org.directory")
  '(package-selected-packages
