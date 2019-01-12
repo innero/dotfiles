f4b1c225870d417f4d0836951e4caf8f0fc324e0
ntkomata <ntkomata@outlook.com>
2019-01-12 14:09:24 +0800

    update zsh structure

---
+++ zsh/.antigen.setup.zsh
@@ -0,0 +1,26 @@
+if [ ! -f ~/.antigen.zsh ]; then
+    curl -L raw.githubusercontent.com/zsh-users/antigen/master/bin/antigen.zsh > ~/.antigen.zsh
+fi
+source ~/.antigen.zsh
+
+# Load the oh-my-zsh's library.
+antigen use oh-my-zsh
+
+
+antigen bundle git
+antigen bundle heroku
+antigen bundle pip
+antigen bundle lein
+antigen bundle command-not-found
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-autosuggestions
+#
+antigen bundle arzzen/calc.plugin.zsh
+
+# Load the theme.
+antigen theme ys
+
+# Tell Antigen that you're done.
+antigen apply
+
+
